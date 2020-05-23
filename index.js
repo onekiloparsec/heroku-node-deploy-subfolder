72e3fbe755b6813c4fa46e51461bab69e68348f7
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-05-23 12:10:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require("@actions/core");
 const { execSync } = require("child_process");
 
 // Support Functions
-const createCatFile = ({ email, api_key }) => `cat >~/.netrc <<EOF
+const createCatFile = (email, api_key) => `cat >~/.netrc <<EOF
 machine api.heroku.com
     login ${email}
     password ${api_key}
@@ -12,12 +12,11 @@ machine git.heroku.com
 EOF`;
 
 // Input Variables
-let heroku = {};
-heroku.api_key = core.getInput("api_key");
-heroku.email = core.getInput("email");
-heroku.app_name = core.getInput("app_name");
-heroku.branch = core.getInput("branch");
-heroku.subfolder = core.getInput("subfolder");
+const api_key = core.getInput("api_key");
+const email = core.getInput("email");
+const app_name = core.getInput("app_name");
+const branch = core.getInput("branch");
+const subfolder = core.getInput("subfolder");
 
 try {
   const isShallow = execSync("git rev-parse --is-shallow-repository").toString();
@@ -26,21 +25,21 @@ try {
     console.log("Successfully unshallowed repo.");
   }
 
-  execSync(createCatFile(heroku));
+  execSync(createCatFile(email, api_key));
   console.log("Successfully wrote to ~./netrc");
 
   execSync("heroku login");
   console.log("Successfully logged into heroku");
 
   execSync(`heroku git:remote --app ${app_name}`);
-  console.log("Successfully set remote.");
+  console.log(`Successfully set remote for ${app_name}.`);
 
   execSync(`
     [ \"\`git rev-parse --abbrev-ref HEAD\`\" == \"${branch}\" ] && git push heroku \`git subtree split --prefix ${subfolder} ${branch}\`:master --force || echo \"No deploy, wrong branch.\""
   `)
   console.log("Deploy successful.");
 
-  core.setOutput("status", `Successfully deployed heroku app from branch ${heroku.branch}`);
+  core.setOutput("status", `Successfully deployed heroku app from branch ${branch}`);
 } catch (err) {
   core.setFailed(err.toString());
 }
